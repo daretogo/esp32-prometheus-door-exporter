ee12b24d870244d8b515a535ee1e283d9eee029e
Aaron Johnson <r.aaron.johnson@gmail.com>
2022-01-03 17:08:01 -0700

    initial commit

---
+++ config.h
@@ -0,0 +1,4 @@
+#define DOOR_SENSOR_PIN 33
+
+const char* ssid = "wifi";
+const char* password = "yourpassword";
---
+++ dorsensor_exporter.ino
@@ -0,0 +1,44 @@
+#include <WiFi.h>
+#include <WebServer.h>
+#include "config.h"
+
+int doorState;
+
+WebServer httpServer(9000);
+
+void handle_request() {
+    doorState = digitalRead(DOOR_SENSOR_PIN); // read state
+    Serial.println(doorState);
+    
+    String HTMLData = "current{label=\"phase1\"} " + String(doorState);
+    httpServer.send(200, "text/html", HTMLData);
+    
+} 
+
+void setup() {
+    Serial.begin(9600);
+    while (!Serial) {}
+    Serial.println("Tring to connect to Wifi SSID: ");
+    Serial.println(ssid);
+  
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(1000);
+        Serial.print(".");
+    }
+    Serial.println("");
+    Serial.println("WiFi connected successfully");
+    Serial.print("Assigned IP: ");
+    Serial.println(WiFi.localIP());
+
+    pinMode(DOOR_SENSOR_PIN, INPUT_PULLUP); // set ESP32 pin to input pull-up mode
+  
+    httpServer.on("/", handle_request);
+    httpServer.begin();
+    Serial.println("HTTP server started");
+    delay(100);
+}
+
+void loop() {
+  httpServer.handleClient();
+}
